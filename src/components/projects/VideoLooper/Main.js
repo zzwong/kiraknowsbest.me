e278ba96d71112f8c1b1900e7aa673c407eaf397
Daniel Metzger <daniel@bittiger.io>
2018-10-05 13:45:55 -0700

    Display code for different components

--- src/components/projects/VideoLooper/Main.js
+++ src/components/projects/VideoLooper/Main.js
@@ -13,6 +13,26 @@ class Main extends React.Component {
     playing: false
   }
 
+  componentDidMount () {
+    this._code.innerText = `
+The code:
+
+
+import React from 'react'
+import YoutubeVideo from './YoutubeVideo'
+import TimeControl from './TimeControl'
+
+class Main extends React.Component {
+  state = {
+    startPosition: 85,
+    endPosition: 642,
+    duration: 0,
+    retryCount: 0,
+    endAt: 0,
+    startAt: 0,
+    playing: false
+  }
+
   // set function that assigns eventListeners
 
   setPlayerSettings = () => {
@@ -105,17 +125,17 @@ class Main extends React.Component {
 
   handleOnTimeUpdate = e => {
     let v = this._video;
-    if (Math.floor(v.currentTime) >= Math.floor(this.state.endAt) || Math.floor(v.currentTime) < Math.floor(this.state.startAt)) {
+    if (Math.floor(v.currentTime) >= Math.floor(this.state.endAt) - 2 || Math.floor(v.currentTime) < Math.floor(this.state.startAt)) {
       v.pause();
       v.currentTime = this.state.startAt;
-      v.play()
+      v.play();
     }
   }
 
   handleOnPause = e => {
     this.setState({playing: false})
     let v = this._video;
-    if (Math.floor(v.currentTime) >= Math.floor(this.state.endAt)) {
+    if (Math.floor(v.currentTime) >= Math.floor(this.state.endAt) - 2) {
       v.currentTime = this.state.startAt;
     } else {
       v.currentTime = Math.max(this.state.startAt, v.currentTime);
@@ -125,7 +145,7 @@ class Main extends React.Component {
   handleOnPlay = e => {
     this.setState({playing: true})
     let v = this._video;
-    if (Math.floor(v.currentTime) >= Math.floor(this.state.endAt)) {
+    if (Math.floor(v.currentTime) >= Math.floor(this.state.endAt) - 2) {
       v.currentTime = this.state.startAt;
     } else {
       v.currentTime = Math.max(this.state.startAt, v.currentTime);
@@ -176,6 +196,280 @@ class Main extends React.Component {
         <div className='video-looper-description'>
           <p>Moving the two orange circles will set the start and stop points for the looping video!</p>
         </div>
+      </div>
+    )
+  }
+}
+
+export default Main;
+
+
+// TimeControl.js
+
+import React from 'react'
+
+class TimeControl extends React.Component {
+  state = {
+    currentlyDragging: false
+  }
+
+  handleMouseDown = () => {
+    this.setState({
+      currentlyDragging: true
+    })
+  }
+
+  handleMouseMove = (e, selection) => {
+    let val = e.pageX - 15;
+    if (this.state.currentlyDragging) {
+      if (val < 85) val = 85;
+      if (val > 642) val = 642;
+      this.props.changeHandler(selection, val);
+    }
+  }
+
+  handleMouseUp = () => {
+    this.setState({
+      currentlyDragging: false
+    })
+  }
+
+  render () {
+    return (
+      <div className='looper-controls'>
+        <div
+          className="looper-selector-wrapper"
+        >
+          <div
+            onMouseMove={e => this.handleMouseMove(e, 'start')}
+            onMouseDown={this.handleMouseDown}
+            onMouseUp={this.handleMouseUp}
+            className="looper-thumb"
+            style={{left: \`` + "${this.props.start}px" + `\`}}
+          />
+          <div
+            onMouseMove={e => this.handleMouseMove(e, 'end')}
+            onMouseDown={this.handleMouseDown}
+            onMouseUp={this.handleMouseUp}
+            className="looper-thumb"
+            style={{left: \`` + "${this.props.end}px" + `\`}}
+          />
+          <div
+            className="looper-selection"
+            style={{left: \`` + "${this.props.start + 15}px" + `\`, width: \`` + "${(this.props.end - this.props.start)}px" + `\`}}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TimeControl
+
+
+// YoutubeVideo.js
+
+import React from 'react'
+
+class YoutubeVideo extends React.Component {
+  componentDidMount () {
+    this.props.ready()
+  }
+
+  render () {
+    return (
+      <div>
+        <div
+          className={\`` + "looper-player${this.props.playing ? null : ' paused'}" + `\`}
+          onClick={this.props.playing ? this.props.pause : this.props.play}
+        >
+          {this.props.playing ? null : <div className='play-icon'></div>}
+          <video
+            width={560}
+            height={315}
+            onPlay={this.props.handleOnPlay}
+            onPause={this.props.handleOnPause}
+            onTimeUpdate={this.props.handleOnTimeUpdate}
+            style={{display: this.props.playing ? 'block' : 'none'}}
+          >
+            <source src="./video/savior.mp4" type="video/mp4"/>
+            Oops, something went wrong. Your browser may not support the video tag.
+          </video>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default YoutubeVideo
+    `
+  }
+  // set function that assigns eventListeners
+
+  setPlayerSettings = () => {
+    // 124 is max duration
+    // startPosition 0 and endPosition 550
+    // should play 0 - 124 seconds
+    // each 4.435483870967742 px is one second
+    const conversion = 4.435483870967742;
+    let v = this._video;
+    let duration = v.duration;
+    let startAt = Math.floor((this.state.startPosition - 85) / conversion);
+    let endAt = Math.ceil((this.state.endPosition - 85) / conversion);
+    this.setState({
+      startAt,
+      endAt
+    })
+  }
+
+  calculateInitialState = () => {
+    this.setState({
+      duration: this._video.duration
+    }, this.setPlayerSettings)
+  }
+
+  captureVideo = () => {
+    let root = document.querySelector('.looper-player');
+    if (!root) {
+      return null;
+    }
+
+    let q = [root];
+
+    while (q.length > 0) {
+      let node = q.shift();
+
+      if (node.tagName.toLowerCase() === 'video') {
+        return node;
+      } else {
+        for (let i = 0; i < node.children.length; i++) {
+          q.push(node.children[i])
+        }
+      }
+    }
+
+    return null;
+  }
+
+  videoIsReady = () => {
+    let vid = this.captureVideo();
+    if (!vid) {
+      if (this.state.retryCount > 15) {
+        this.setState({
+          errorGettingVideo: true
+        });
+      } else {
+        this.setState({
+          retryCount: this.state.retryCount + 1
+        });
+        setTimeout(() => {
+          this.videoIsReady();
+        }, 750);
+      }
+    } else {
+      this._video = vid;
+      setTimeout(() => {
+        this.calculateInitialState();
+      }, 500)
+    }
+  }
+
+  updateDuration = val => this.setState({duration: val})
+
+  changeHandler = (selection, value) => {
+    let newStart, newEnd;
+    let currStart = this.state.startPosition;
+    let currEnd = this.state.endPosition;
+    if (selection === 'start') {
+      newStart = Math.max(85, value);
+      newEnd = Math.min(642, Math.max(newStart + 10, currEnd));
+    } else {
+      newEnd = Math.min(642, value);
+      newStart = Math.max(85, Math.min(newEnd - 10, currStart));
+    }
+
+    this.setState({
+      startPosition: newStart,
+      endPosition: newEnd
+    }, this.setPlayerSettings)
+  }
+
+  handleOnTimeUpdate = e => {
+    let v = this._video;
+    if (Math.floor(v.currentTime) >= Math.floor(this.state.endAt) - 2 || Math.floor(v.currentTime) < Math.floor(this.state.startAt)) {
+      v.pause();
+      v.currentTime = this.state.startAt;
+      v.play();
+    }
+  }
+
+  handleOnPause = e => {
+    this.setState({playing: false})
+    let v = this._video;
+    if (Math.floor(v.currentTime) >= Math.floor(this.state.endAt) - 2) {
+      v.currentTime = this.state.startAt;
+    } else {
+      v.currentTime = Math.max(this.state.startAt, v.currentTime);
+    }
+  }
+
+  handleOnPlay = e => {
+    this.setState({playing: true})
+    let v = this._video;
+    if (Math.floor(v.currentTime) >= Math.floor(this.state.endAt) - 2) {
+      v.currentTime = this.state.startAt;
+    } else {
+      v.currentTime = Math.max(this.state.startAt, v.currentTime);
+    }
+  }
+
+  pause = () => {
+    let v = this._video;
+    v.pause();
+    this.setState({
+      playing: false
+    });
+  }
+
+  play = () => {
+    const instance = this;
+    try {
+      let v = this._video;
+      v.play();
+    }
+    catch (e) {
+      console.error(e)
+    }
+    finally {
+      this.setState({
+        playing: true
+      });
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <div className='video-looper-container'>
+          <YoutubeVideo
+            ready={this.videoIsReady}
+            handleOnPlay={this.handleOnPlay}
+            handleOnPause={this.handleOnPause}
+            handleOnTimeUpdate={this.handleOnTimeUpdate}
+            playing={this.state.playing}
+            play={this.play}
+            pause={this.pause}
+          />
+          <TimeControl
+            changeHandler={this.changeHandler}
+            start={this.state.startPosition}
+            end={this.state.endPosition}
+          />
+          <div className='video-looper-description'>
+            <p>Moving the two orange circles will set the start and stop points for the looping video!</p>
+          </div>
+        </div>
+        <div ref={el => this._code = el} className="code-display"/>
       </div>
     )
   }
--- src/components/stepSequencer/MusicInterface.js
+++ src/components/stepSequencer/MusicInterface.js
@@ -35,6 +35,183 @@ class MusicInterface extends React.Component {
     this._clap = document.getElementById('clap-audio')
     this._snare = document.getElementById('snare-audio')
     document.body.addEventListener('keydown', this.setSpacebarToPlay, true)
+    this._code.innerText = `
+The code:
+
+
+import React from 'react'
+
+class MusicInterface extends React.Component {
+  state = {
+    playing: false,
+    intervalId: null,
+    speed: 140,
+    instrumentsToPlay: [['kick'], [], ['hihat'], [], ['kick', 'snare'], [], ['hihat'], []]
+  }
+
+  setSpacebarToPlay = e => {
+    if (e.key === ' ') {
+      this.play()
+    }
+  }
+
+  toggleActiveClass = e => {
+    let key = e.target.dataset.key;
+    let step = parseInt(e.target.dataset.step);
+    let newInstrumentsToPlay = this.state.instrumentsToPlay.slice(0);
+    if (e.target.classList.contains('music-active')) {
+      newInstrumentsToPlay[step-1].splice(newInstrumentsToPlay[step-1].indexOf(key), 1)
+    } else {
+      newInstrumentsToPlay[step-1].push(key);
+    }
+    this.setState({
+      instrumentsToPlay: newInstrumentsToPlay
+    });
+    e.target.classList.toggle('music-active')
+  }
+
+  componentDidMount = () => {
+    this._kick = document.getElementById('kick-audio')
+    this._hihat = document.getElementById('hihat-audio')
+    this._clap = document.getElementById('clap-audio')
+    this._snare = document.getElementById('snare-audio')
+    document.body.addEventListener('keydown', this.setSpacebarToPlay, true)
+  }
+
+  componentWillUnmount = () => {
+    document.body.removeEventListener('keydown', this.setSpacebarToPlay, true)
+    if (this.state.playing) this.play()
+  }
+
+  play = () => {
+    if (this.state.playing) {
+      this.clickOnPause(0);
+    } else {
+      let currentStep = 0;
+      let intervalId = setInterval(() => {
+        if (currentStep === 8) currentStep = 0;
+        for (let i = 0; i < this.state.instrumentsToPlay[currentStep].length; i++) {
+          let sound = this[\`` + "_${this.state.instrumentsToPlay[currentStep][i]}" + `\`];
+          sound.currentTime = 0;
+          sound.play();
+        }
+        currentStep++;
+      }, this.state.speed)
+      this.setState({
+        playing: true,
+        intervalId: intervalId
+      })
+    }
+  }
+
+  clickOnPause = playAfter => {
+    window.clearInterval(this.state.intervalId)
+    this.setState({
+      playing: false,
+      intervalId: null,
+    }, () => {
+      if (playAfter) this.play()
+    })
+  }
+
+  slowDown = () => {
+    let wasPlaying = this.state.playing
+    let newSpeed = this.state.speed + 5
+    if (1050 - newSpeed < 1) newSpeed = 1049
+    this.setState({
+      speed: newSpeed
+    }, () => {
+      this.clickOnPause(wasPlaying)
+    })
+  }
+
+  speedUp = () => {
+    let wasPlaying = this.state.playing
+    let newSpeed = this.state.speed - 5
+    if (1050 - newSpeed > 1000) newSpeed = 50
+    this.setState({
+      speed: newSpeed
+    }, () => {
+      this.clickOnPause(wasPlaying)
+    })
+  }
+
+  slideSpeed = e => {
+    let val = 1050 - e.target.value;
+    let wasPlaying = this.state.playing;
+    this.setState({
+      speed: val
+    }, () => {
+      this.clickOnPause(wasPlaying)
+    })
+  }
+
+  render () {
+    return (
+      <div className="music-main-wrapper">
+        <h2 className='music-heading'>Step Sequencer</h2>
+        <div className='music-description'>
+          <p>Click play to hear the music! Feel free to change the instruments around and change the tempo, too!</p>
+          <p>You can also switch between play / pause by using the [spacebar] .</p>
+          <p>You can use the arrow buttons to change the speed a little bit, or use the slider to change the speed by a large amount!</p>
+        </div>
+        <div className="music-control-panel">
+          <div onClick={this.play} className={\`` + "music-${this.state.playing ? 'pause' : 'play'}-btn music-btn" + `\`}>{this.state.playing ? 'Pause' : 'Play'}</div>
+          <div onClick={this.slowDown} className="music-slow-btn music-btn">{'<<'}</div>
+          <div onClick={this.speedUp} className="music-fast-btn music-btn">{'>>'}</div>
+          <input onChange={this.slideSpeed} type="range" min="1" max="1000" value={1050 - this.state.speed} className="music-slider" id="speedRange"/>
+        </div>
+        <div className="music-step-row">
+          <div className="music-track-name">Kick</div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step1 music-active" data-step="1" data-key="kick"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step2" data-step="2" data-key="kick"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step3" data-step="3" data-key="kick"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step4" data-step="4" data-key="kick"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step5 music-active" data-step="5" data-key="kick"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step6" data-step="6" data-key="kick"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step7" data-step="7" data-key="kick"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step8" data-step="8" data-key="kick"></div>
+        </div>
+        <div className="music-step-row">
+          <div className="music-track-name">Hihat</div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step1" data-step="1" data-key="hihat"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step2" data-step="2" data-key="hihat"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step3 music-active" data-step="3" data-key="hihat"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step4" data-step="4" data-key="hihat"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step5" data-step="5" data-key="hihat"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step6" data-step="6" data-key="hihat"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step7 music-active" data-step="7" data-key="hihat"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step8" data-step="8" data-key="hihat"></div>
+        </div>
+        <div className="music-step-row">
+          <div className="music-track-name">Clap</div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step1" data-step="1" data-key="clap"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step2" data-step="2" data-key="clap"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step3" data-step="3" data-key="clap"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step4" data-step="4" data-key="clap"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step5" data-step="5" data-key="clap"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step6" data-step="6" data-key="clap"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step7" data-step="7" data-key="clap"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step8" data-step="8" data-key="clap"></div>
+        </div>
+        <div className="music-step-row">
+          <div className="music-track-name">Snare</div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step1" data-step="1" data-key="snare"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step2" data-step="2" data-key="snare"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step3" data-step="3" data-key="snare"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step4" data-step="4" data-key="snare"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step5 music-active" data-step="5" data-key="snare"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step6" data-step="6" data-key="snare"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step7" data-step="7" data-key="snare"></div>
+          <div onClick={this.toggleActiveClass} className="music-step music-step8" data-step="8" data-key="snare"></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MusicInterface
+    `
   }
 
   componentWillUnmount = () => {
@@ -164,6 +341,7 @@ class MusicInterface extends React.Component {
           <div onClick={this.toggleActiveClass} className="music-step music-step7" data-step="7" data-key="snare"></div>
           <div onClick={this.toggleActiveClass} className="music-step music-step8" data-step="8" data-key="snare"></div>
         </div>
+        <div ref={el => this._code = el} className="code-display"/>
       </div>
     )
   }
